17df7e25cde729d7d3f0ed0a22be7163f085d713
Moritz Lawitschka <moritz@urbanvention.com>
2014-01-24 13:51:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuitepadDoc::Application.config.secret_key_base = '289e485950c43f7ba551e70fe7f71fb985865b6adaa89b16d8083d79868798b1640be721a195afae9fe19fecffbdc4c961fb81257e2abbe81a76c49af305beda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuitepadDoc::Application.config.session_store :cookie_store, key: '_suitepad_doc_session'
